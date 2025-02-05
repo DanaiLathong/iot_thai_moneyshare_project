bbd570314bcb347e9ed30178bd41d52e31c9670d
DanaiLathong <bankkok159z@gmail.com>
2025-02-05 12:43:30 +0700

    V.2 : Add SplashScreen and MoneyUi FINISHH!!!

--- lib/view/money_input_ui.dart
+++ lib/view/money_input_ui.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: sort_child_properties_last
+
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -9,6 +11,9 @@ class MoneyInputUI extends StatefulWidget {
 }
 
 class _MoneyInputUIState extends State<MoneyInputUI> {
+  //ตัวแปรใช้กับ Checkbox
+  bool isTip = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,69 +29,189 @@ class _MoneyInputUIState extends State<MoneyInputUI> {
         centerTitle: true,
       ),
       body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            children: [
-              SizedBox(
-                height: 50.0,
-              ),
-              Image.asset(
-                'assets/images/money.png',
-                width: MediaQuery.of(context).size.width * 0.35,
-              ),
-              SizedBox(
-                height: 35.0,
-              ),
-              TextField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(
+        child: Padding(
+          padding: EdgeInsets.only(
+            left: 45.0,
+            right: 45.0,
+          ),
+          child: Center(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 50.0,
+                ),
+                Image.asset(
+                  'assets/images/money.png',
+                  width: MediaQuery.of(context).size.width * 0.35,
+                ),
+                SizedBox(
+                  height: 35.0,
+                ),
+                TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.purple,
+                      ),
+                    ),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.purple,
+                      ),
+                    ),
+                    prefixIcon: Icon(
+                      FontAwesomeIcons.moneyBill1Wave,
                       color: Colors.purple,
                     ),
-                  ),
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.purple,
+                    hintText: 'ป้อนจำนวนเงิน (บาท)',
+                    hintStyle: TextStyle(
+                      color: Colors.grey[400],
                     ),
-                  ),
-                  prefixIcon: Icon(
-                    FontAwesomeIcons.moneyBill1Wave,
-                    color: Colors.purple,
-                  ),
-                  hintText: 'ป้อนจำนวนเงิน (บาท)',
-                  hintStyle: TextStyle(
-                    color: Colors.grey[400],
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 35.0,
-              ),
-              TextField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(
+                SizedBox(
+                  height: 35.0,
+                ),
+                TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.purple,
+                      ),
+                    ),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.purple,
+                      ),
+                    ),
+                    prefixIcon: Icon(
+                      Icons.person,
                       color: Colors.purple,
                     ),
-                  ),
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.purple,
+                    hintText: 'ป้อนจำนวนคน (คน)',
+                    hintStyle: TextStyle(
+                      color: Colors.grey[400],
                     ),
-                  ),
-                  prefixIcon: Icon(
-                    Icons.person,
-                    color: Colors.purple,
-                  ),
-                  hintText: 'ป้อนจำนวนคน (คน)',
-                  hintStyle: TextStyle(
-                    color: Colors.grey[400],
                   ),
                 ),
-              ),
-            ],
+                SizedBox(
+                  height: 35.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Checkbox(
+                      onChanged: (paramValue) {
+                        //จำไว้นะ โค้ดใดที่มีผลต่อการแสดงผล
+                        //ต้องเขียนอยู่ภายใต้ setState()
+                        setState(() {
+                          isTip = paramValue!;
+                        });
+                      },
+                      value: isTip,
+                      activeColor: Colors.purple,
+                      checkColor: Colors.white,
+                      side: BorderSide(
+                        color: Colors.purple,
+                      ),
+                    ),
+                    Text(
+                      'ทิปให้พนักงานเสริฟ',
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 22.0,
+                ),
+                TextField(
+                  enabled: isTip,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.purple,
+                      ),
+                    ),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.purple,
+                      ),
+                    ),
+                    prefixIcon: Icon(
+                      FontAwesomeIcons.coins,
+                      color: Colors.purple,
+                    ),
+                    hintText: 'ป้อนจำนวนเงินทิป (บาท)',
+                    hintStyle: TextStyle(
+                      color: Colors.grey[400],
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 35.0,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'คำนวณ',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.purple,
+                    fixedSize: Size(
+                      MediaQuery.of(context).size.width,
+                      50.0,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(
+                        8.0,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 15.0,
+                ),
+                ElevatedButton.icon(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.cancel_outlined,
+                    color: Colors.white,
+                  ),
+                  label: Text(
+                    'ยกเลิก',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 180, 56, 47),
+                    fixedSize: Size(
+                      MediaQuery.of(context).size.width,
+                      50.0,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(
+                        8.0,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 35.0,
+                ),
+                Text(
+                  'Create by NinniN SAU',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
