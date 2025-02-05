85782d95069081a71762a7e991b7e9f41895e681
DanaiLathong <bankkok159z@gmail.com>
2025-02-05 12:39:02 +0700

    V.12 : สเพิ่ม ฆsplashscreen และ inputUi เรียบร้อย

---
+++ lib/view/money_input_ui.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class MoneyInputUI extends StatefulWidget {
+  const MoneyInputUI({super.key});
+
+  @override
+  State<MoneyInputUI> createState() => _MoneyInputUIState();
+}
+
+class _MoneyInputUIState extends State<MoneyInputUI> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 242, 197, 249),
+      appBar: AppBar(
+        backgroundColor: Colors.purple,
+        title: Text(
+          'แชร์เงินกันเถอะ',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 50.0,
+              ),
+              Image.asset(
+                'assets/images/money.png',
+                width: MediaQuery.of(context).size.width * 0.35,
+              ),
+              SizedBox(
+                height: 35.0,
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.purple,
+                    ),
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.purple,
+                    ),
+                  ),
+                  prefixIcon: Icon(
+                    FontAwesomeIcons.moneyBill1Wave,
+                    color: Colors.purple,
+                  ),
+                  hintText: 'ป้อนจำนวนเงิน (บาท)',
+                  hintStyle: TextStyle(
+                    color: Colors.grey[400],
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 35.0,
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.purple,
+                    ),
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.purple,
+                    ),
+                  ),
+                  prefixIcon: Icon(
+                    Icons.person,
+                    color: Colors.purple,
+                  ),
+                  hintText: 'ป้อนจำนวนคน (คน)',
+                  hintStyle: TextStyle(
+                    color: Colors.grey[400],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
